f935ab464c5b596851581ac2406a13982d4ae938
Daniel Kurek <daniel.kurek.dev@gmail.com>
2022-07-28 17:47:18 +0200

    added best score

--- dino.py
+++ dino.py
@@ -5,6 +5,9 @@ import time
 from game_engine import *
 import random
 
+# file access
+import io
+
 piezo = Pin(9, Pin.OUT)
 
 button_0 = Pin(0, Pin.IN, Pin.PULL_DOWN)
@@ -26,6 +29,8 @@ spi = SPI(1, 80_000_000, sck=Pin(10), mosi=Pin(11))
 display_width = 128
 display_height = 64
 display = ssd1306.SSD1306_SPI(display_width, display_height, spi, dc=Pin(8), res=Pin(7), cs=Pin(9))
+
+best_score_path = "/best_score.txt"
 
 # load images
 dino_path = "/images/dino-cropped-20-22.pbm"
@@ -49,11 +54,19 @@ obstacles = [cactus]
 cactus.set_motion_vector(-1.5,0)
 
 def game_loop():
+    global score
+
     now = time.ticks_ms()
+    # update time in physics loop
     player.physics_tick(now)
-    player.set_pos(10, display_height - dino_img.height)
     cactus.physics_tick(now)
+
+    # reset position
+    player.set_pos(10, display_height - dino_img.height)
     cactus.set_pos(120, display_height - cactus_img.height)
+
+    score = 0
+
     while True:
         display.fill(0)
         now = time.ticks_ms()
@@ -61,23 +74,43 @@ def game_loop():
             player.set_motion_vector(0,-3.5)
         if cactus.x <= -cactus_img.width :
             cactus.set_pos(x=128 + random.randint(0, 128))
+            score += 1 # original game counts score as a distance travelled
         for o in objects:
             o.physics_tick(now)
         for o in objects:
             o.draw()
+        display.text(str(score), 64, 0)
         display.show()
         if player.collision_test(obstacles) != None:
             break
 
 def start_text():
-    display.text("Press ACT", 0, 0, 1)
+    display.text("Press ACT", 25, 20, 1)
+    display.text("Best: " + str(best_score), 32, 10, 1)
     display.show()
+
+best_score = 0
+
+try:
+    with io.open(best_score_path, "r") as f:
+        try:
+            best_score = int(f.readline())
+        except ValueError:
+            print("cannot read best score")
+except OSError:
+    print("file not found")
+    
 
 start_text()
 
 while True:
     if button_action.value() == 1:
         game_loop()
+        if score != None and score > best_score:
+            # add animation or other indication of new best score
+            best_score = score
+            with io.open(best_score_path, "w") as f:
+                f.write(str(score))
         start_text()
     
 
